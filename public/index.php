560974ebdcd8c06a4b51d39d5efc660fcefa6873
Vlad Cociorva <skyehigh@Vlads-MacBook-Pro.local>
2021-03-30 09:26:36 +0300

    added public and template folders

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ReDat</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body class = "index-body">
+<?php
+    include ("../templates/navbar.php");
+?>
+
+<main>
+    <h1> TODO </h1>
+</main>
+
+<?php
+    include ("../templates/footer.php");
+?>
+
+</body>
+
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class = "footer-title">
+        <p class = footer-title-p>
+            ReDAT &copy;
+        </p>
+    </div>
+    <div class = "footer-contact">
+        <a href="#" class="contact-button"> Contact us </a>
+        <a href="#" class="github-button"> <i class="fab fa-github"></i> Github </a>
+    </div>
+</footer>
