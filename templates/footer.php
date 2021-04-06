3d6bc27468544526b46fa485f71ab0c5b59a0955
Rares Radu <radu.rares.a@gmail.com>
2021-04-06 20:48:27 +0300

    +work on front-end

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
         </p>
     </div>
     <div class = "footer-contact">
-        <a href="#" class="contact-button"> Contact us </a>
-        <a href="#" class="github-button"> <i class="fab fa-github"></i> Github </a>
+        <a href="#" class="footer-button"> Contact us </a>
+        <a href="#" class="footer-button"> <i class="fab fa-github"></i> Github </a>
     </div>
 </footer>
