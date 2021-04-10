e19d1d8d3ea047094990914578914b4e0b55baed
Vlad Cociorva <skyehigh@Vlads-MacBook-Pro.local>
2021-04-10 14:29:07 +0300

    + sign-in page

--- public/sign_in.php
+++ public/sign_in.php
@@ -5,26 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ReDat</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/sign_in.css">
 </head>
 
-<body class = "login-body">
+<body>
 
 <main>
-    <h1 class = login-greet>
-        Hello!
-    </h1>
+    <div id="container">
 
-    <p>
+        <div class="left-panel">
+            <div class="description">
+                <h1>ReDAT</h1>
+                <p>You can use ReDAT to monitor your favorite subreddits and get various statistics about posts, users and more. </p>
+            </div>
+        </div>
 
-    </p>
+        <div class="right-panel">
+            <button class="sign-in-btn" type="submit"> Sign In</button>
+            <section class="description">
+                <p><span class="small">You will be redirected to reddit.com for authentication.</span></p>
+            </section>
+        </div>
+
+    </div>
 
 </main>
 
-
 </body>
-<?php
-    include ("../templates/footer.php");
-?>
 
 </html>
