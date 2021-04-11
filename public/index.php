3c0869a19371c261ec1ab47c38f69d87c5b216e5
Rares Radu <radu.rares.a@gmail.com>
2021-04-11 18:59:47 +0300

    + selectors for graphs

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ReDat</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" >
     <link rel="stylesheet" href="css/main.css">
     <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 </head>
